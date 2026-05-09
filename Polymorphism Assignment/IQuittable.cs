2cff05a8ff1de77324efb8b6d1f2393f6a5a55d7
yahya-bader-khawam <58319863+yahya-bader-khawam@users.noreply.github.com>
2026-05-09 10:42:42 -0400

    Create IQuittable.cs

---
+++ Polymorphism Assignment/IQuittable.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PolymorphismAssignment
+{
+    // 1. Create an interface called IQuittable.
+    interface IQuittable
+    {
+        // 1 (cont). Define a void method called Quit().
+        void Quit();
+    }
+}
